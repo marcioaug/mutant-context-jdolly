57f9dc0dfac55ae9c8ef32959b9d62cfcab7abea
Marcio Augusto Guiamarães <marcioaugustosg@gmail.com>
2018-03-13 23:38:16 -0300

    stable version

--- gen/__main2__.py
+++ gen/__main2__.py
@@ -1,10 +1,16 @@
 import os
 import shutil
 import subprocess
+import re
+
 
 INPUT_DIR = os.path.join('jdolly_source', '0')
 OUTPUT_DIR = os.path.join('data')
 SOOT_JAR = os.path.join('soot', 'soot.jar')
+
+
+def sort_files(files):
+    return sorted(files, key=lambda x: (int(0 if re.sub(r'[^0-9]+','',x) == '' else re.sub(r'[^0-9]+','',x)),x))
 
 
 def get_files(path, ext='.java'):
@@ -36,20 +42,16 @@ def get_class_files(path, package='', ext='.class'):
     return files
 
 
-def compile(classpath, class_file=None):
+def compile(classpath, program_dir, files):
 
-    files = []
-
-    if class_file != None:
-        files.append(class_file)
-    else:
-        files = get_files(classpath)
+    command = ['javac', '-cp', classpath]
 
     for input_file in files:
-        command = ['javac', '-cp', classpath, input_file]
-        subprocess.call(command, shell=False, 
-                            stdout=subprocess.DEVNULL, 
-                            stderr=subprocess.DEVNULL)
+        command.append(os.path.join(program_dir, input_file))
+
+    return subprocess.call(command, shell=False, 
+                                stdout=subprocess.DEVNULL, 
+                                stderr=subprocess.DEVNULL)
 
 
 def exec_major(class_file, classpath, mutants_dir):
@@ -77,20 +79,26 @@ def exec_major(class_file, classpath, mutants_dir):
                                 stderr=subprocess.DEVNULL)
 
 
-def soot_optmizer(classpath, program_dir, java_file, class_file):
+def soot_optmizer(classpath, program_dir, java_files):
     soot_jar = os.path.abspath(SOOT_JAR)
 
-    compile(classpath, java_file)
+    return_code = compile(classpath, program_dir, java_files)
 
-    rt_jar = '/usr/lib/jvm/default-jvm/jre/lib/rt.jar'
+    for java_file in java_files:
+            class_file = java_file.split('.')[0].replace(os.sep, '.')
+            java_file = os.path.join(program_dir, java_file)
 
-    classpath = program_dir + ':' + rt_jar + ':' + classpath
+            rt_jar = '/usr/lib/jvm/default-jvm/jre/lib/rt.jar'
 
-    command = ['java', '-jar', soot_jar, '-cp', classpath, '-O', class_file]
+            classpath = program_dir + ':' + rt_jar + ':' + classpath
 
-    return subprocess.call(command, shell=False, cwd=program_dir, 
+            command = ['java', '-jar', soot_jar, '-cp', classpath, '-O', class_file]
+
+            subprocess.call(command, shell=False, cwd=program_dir, 
                                 stdout=subprocess.DEVNULL, 
                                 stderr=subprocess.DEVNULL)
+    
+    return return_code
 
 
 def diff(mutant_dir, original_dir):
@@ -114,6 +122,8 @@ def diff(mutant_dir, original_dir):
 
 
 def generate_mutants(test, classpath, working_dir):
+    print('-' * 80)
+    print('> GENERATING MUTANTS AND CHECKING EQUIVALENCE FOR %s...' % test)
 
     full_qualified_java = get_class_files(classpath, ext='.java')
 
@@ -131,26 +141,43 @@ def generate_mutants(test, classpath, working_dir):
         csv = open(os.path.join(mutants_dir, 'tce.csv'), 'w')
         csv.write('test,mutantNo,status\n')
 
-        for mutant in os.listdir(mutants_dir):
+        total_mutants = len([dir for dir in os.listdir(mutants_dir) if os.path.isdir(os.path.join(mutants_dir, dir))])
+
+        print('%s -> %d MUTANTS GENERATED FOR %s' % (test, total_mutants, java_file))
+
+        count = 1
+        equivante_count = 0
+
+        for mutant in sort_files(os.listdir(mutants_dir)):            
             mutant_dir = os.path.join(mutants_dir, mutant)
             if os.path.isdir(mutant_dir):
-                mutant_java = os.path.join(mutant_dir, java_file)
-                mutant_class = java_file.split('.')[0].replace(os.sep, '.')
-                soot_optmizer(classpath, mutant_dir, mutant_java, mutant_class)
-                if (diff(mutant_dir, classpath)):
-                    print ("!!!! Mutant " + mutant_java + " is equivalent.")
-                    csv.write(test + ',' + mutant + ',' + 'TCE_CONFIRMED\n')
+                print('%s -> RUNNING TCE FOR %d OF %d MUTANTS.' % (test, count, total_mutants))
+                count += 1
+                
+                if soot_optmizer(classpath, mutant_dir, [java_file]) == 0:
+                    if (diff(mutant_dir, classpath)):
+                        
+                        print ('!!!! %s -> MUTANT %s (%s) IS EQUIVALENT. %s' % (test, mutant, java_file, ('< ' * 10)))
+                        
+                        equivante_count += 1
+                        csv.write(test + ',' + mutant + ',' + 'TCE_CONFIRMED\n')
+                    else:
+                        csv.write(test + ',' + mutant + ',' + 'NOT_CONFIRMED\n')
                 else:
-                    csv.write(test + ',' + mutant + ',' + 'NO\n')
+                    print('%s ERROR -> MUTANT %s DONT COMPILE.' % (test, mutant))
+                    csv.write(test + ',' + mutant + ',' + 'DONT_COMPILE\n')
         
         csv.close()
+
+        print('EQUIVALENCE ANALISIS FOR %s FINISH. %d EQUIVALENT(S) OF %d' % (test, equivante_count, total_mutants))
+        print('-' * 80)
 
 
 def copy_original(src, dest, test):
     dest_path = os.path.join(dest, test, 'original')
 
     if not os.path.exists(dest_path):
-        shutil.copytree(os.path.join(src, test), dest_path)
+        shutil.copytree(os.path.join(src, test, 'in'), dest_path)
         return dest_path
 
     return None   
@@ -160,7 +187,7 @@ def main():
     input_dir = os.path.abspath(INPUT_DIR)
     output_dir = os.path.abspath(OUTPUT_DIR)
 
-    for test_dir in os.listdir(input_dir):
+    for test_dir in sort_files(os.listdir(input_dir)):
         if os.path.isdir(os.path.join(input_dir, test_dir)):         
             working_dir = os.path.join(output_dir, test_dir)
             original_dir = copy_original(input_dir, output_dir, test_dir)
@@ -168,12 +195,9 @@ def main():
             if original_dir != None:     
                 full_qualified_java = get_class_files(original_dir, ext='.java')
 
-                for java_file in full_qualified_java:
-                    class_file = java_file.split('.')[0].replace(os.sep, '.')
-                    java_file = os.path.join(original_dir, java_file)
-
-                    soot_optmizer(original_dir, original_dir, java_file, class_file)
-
-                generate_mutants(test_dir, original_dir, working_dir)
+                if soot_optmizer(original_dir, original_dir, full_qualified_java) != 0:
+                    shutil.rmtree(os.path.join(output_dir, test_dir))    
+                else:
+                    generate_mutants(test_dir, original_dir, working_dir)
             else:
                 print("SKIPING " + test_dir)    
